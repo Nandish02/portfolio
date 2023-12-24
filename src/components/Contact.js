02a3ee94593237ea9986cd10d1eb5d7ca5be4928
Nandish02 <f20202031@goa.bits-pilani.ac.in>
2023-12-25 00:36:31 +0530

    Contact Page Updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,15 +21,18 @@ function Contact() {
         `}
       </style>
       <div className="max-w-xl mx-auto mt-10 sm:mt-20 overflow-y-auto">
-        <h1 className="text-5xl font-bold mb-8">
-          <span className="border-b-4 border-pink-500">Contact Me!</span>
+        <h1 className="text-5xl font-bold mb-8 text-center"> {/* Added 'text-center' class for centering */}
+          <span className="border-b-4 border-pink-500">Contact</span>
         </h1>
         <div className="mb-6">
-          {/* Include the Google Form iframe */}
+          {/* Adjust width and height for a balanced appearance */}
           <iframe
+            title="Contact Form"
+            className="w-full h-[600px] md:w-[400px] md:h-[800px] lg:w-[500px] lg:h-[1000px] xl:w-[600px] xl:h-[1200px]"
             src="https://docs.google.com/forms/d/e/1FAIpQLSdvNKoXR9L2x6mbWPOgxSttwtHz-tLFBgDSIq9-OT5NTFoPBw/viewform?embedded=true"
-            height="618"
-            width="640"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
           >
             Loading...
           </iframe>
@@ -40,7 +43,3 @@ function Contact() {
 }
 
 export default Contact;
-
-
-
-
